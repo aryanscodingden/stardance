4025d20580e534663715e7d7ab3e7089c812417f
nullskulls <nullskulls@proton.me>
2026-07-05 02:32:52 +0300

    chore(migrations): fold proof_video_url into the dashboard tracking migration

--- db/migrate/20260702222701_add_external_dashboard_tracking_to_certification_ship_reviews.rb
+++ db/migrate/20260702222701_add_external_dashboard_tracking_to_certification_ship_reviews.rb
@@ -3,6 +3,7 @@ class AddExternalDashboardTrackingToCertificationShipReviews < ActiveRecord::Mig
 
   def change
     add_column :certification_ship_reviews, :external_certification_id, :string
+    add_column :certification_ship_reviews, :proof_video_url, :string
     add_index :certification_ship_reviews, :external_certification_id, unique: true, algorithm: :concurrently
     add_reference :certification_ship_reviews, :post_ship_event, index: { algorithm: :concurrently }
     add_foreign_key :certification_ship_reviews, :post_ship_events, column: :post_ship_event_id, on_delete: :nullify, validate: false
--- db/migrate/20260704231353_add_proof_video_url_to_certification_ship_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddProofVideoUrlToCertificationShipReviews < ActiveRecord::Migration[8.1]
-  def change
-    add_column :certification_ship_reviews, :proof_video_url, :string
-  end
-end
